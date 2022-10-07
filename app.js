0992533739973123b2b17e08190e3e72d75d1a89
TheGeogeo <coudrier.g@gmail.com>
2022-10-07 11:29:07 +0200

    ini

--- app.js
+++ app.js
@@ -2,12 +2,8 @@ import dragons from "./assets/dragons.js"
 
 const drag = dragons;
 
-function coucou(){
-    console.log("coucou");
-}
-
 document.addEventListener("DOMContentLoaded", (event) => {
     
-    document.getElementById("btn").addEventListener("click", coucou)
+    alert("iii")
  
 });
